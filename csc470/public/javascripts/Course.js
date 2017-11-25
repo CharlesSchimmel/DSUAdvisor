05d1ea294740121879feca2bbda0a9c67b8ad6d1
Catherine Oborski <catherine.oborski@mines.sdsmt.edu>
2017-11-24 20:38:08 -0700

    fleshed out courses namespace

--- csc470/public/javascripts/Course.js
+++ csc470/public/javascripts/Course.js
@@ -1,12 +1,100 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 var courses;
 (function (courses) {
+    var MINYEAR = 2000;
+    var MAXYEAR = 2040;
     var Course = (function () {
         function Course() {
         }
+        //--------
+        Course.prototype.getClassId = function () {
+            return this._classId;
+        };
+        Course.prototype.getName = function () {
+            return this._name;
+        };
+        Course.prototype.setName = function (name) {
+            this._name = name;
+        };
+        Course.prototype.getCourseNumber = function () {
+            return this._courseNumber;
+        };
+        Course.prototype.setCourseNumber = function (courseNumber) {
+            this._courseNumber = courseNumber;
+        };
+        Course.prototype.getCreditHours = function () {
+            return this._creditHours;
+        };
+        Course.prototype.setCreditHours = function (creditHours) {
+            if (creditHours > 0 && creditHours < 6) {
+                this._creditHours = creditHours;
+            }
+        };
+        Course.prototype.getSubject = function () {
+            return this._subject;
+        };
+        Course.prototype.setSubject = function (subject) {
+            this._subject = subject;
+        };
+        Course.prototype.getDescription = function () {
+            return this._description;
+        };
+        Course.prototype.setDescription = function (description) {
+            this._description = description;
+        };
+        Course.prototype.getPreRequisites = function () {
+            return this._preRequisites;
+        };
+        Course.prototype.setPreRequisites = function (preRequ) {
+            this._preRequisites = preRequ;
+        };
+        Course.prototype.addPreRequisite = function (requ) {
+            this._preRequisites[requ.getName()] = requ;
+        };
+        Course.prototype.removePreRequisite = function (requ) {
+            delete this._preRequisites[requ];
+        };
         return Course;
     }());
+    var CourseInstance = (function (_super) {
+        __extends(CourseInstance, _super);
+        function CourseInstance() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        //------------------
+        CourseInstance.prototype.getProfessor = function () {
+            return this._professor;
+        };
+        CourseInstance.prototype.setProfessor = function (professor) {
+            this._professor = professor;
+        };
+        CourseInstance.prototype.getSemester = function () {
+            return this._semester;
+        };
+        CourseInstance.prototype.setSemester = function (semester) {
+            this._semester = semester;
+        };
+        CourseInstance.prototype.getYear = function () {
+            return this._year;
+        };
+        CourseInstance.prototype.setYear = function (year) {
+            if (year > MINYEAR && year < MAXYEAR) {
+                this._year = year;
+            }
+        };
+        return CourseInstance;
+    }(Course));
     var Subject;
     (function (Subject) {
         Subject[Subject["ENG"] = 0] = "ENG";
--- csc470/public/javascripts/Course.ts
+++ csc470/public/javascripts/Course.ts
@@ -1,6 +1,9 @@
 ﻿import Id = require('./Id');
 
 namespace courses {
+    const MINYEAR = 2000;
+    const MAXYEAR = 2040;
+
     class Course {
         private _classId: number;
         private _name: string;
@@ -8,6 +11,97 @@ namespace courses {
         private _creditHours: number;
         private _subject;
         private _description: string;
+        private _preRequisites;
+
+        //--------
+
+        public getClassId() {
+            return this._classId;
+        }
+
+        public getName() {
+            return this._name;
+        }
+        public setName(name: string) {
+            this._name = name;
+        }
+
+        public getCourseNumber() {
+            return this._courseNumber;
+        }
+        public setCourseNumber(courseNumber: string) {
+            this._courseNumber = courseNumber;
+        }
+
+        public getCreditHours() {
+            return this._creditHours;
+        }
+        public setCreditHours(creditHours: number) {
+            if (creditHours > 0 && creditHours < 6) {
+                this._creditHours = creditHours;
+            }
+        }
+
+        public getSubject() {
+            return this._subject;
+        }
+        public setSubject(subject: Subject) {
+            this._subject = subject;
+        }
+
+        public getDescription() {
+            return this._description;
+        }
+        public setDescription(description: string) {
+            this._description = description;
+        }
+
+        public getPreRequisites() {
+            return this._preRequisites;
+        }
+        public setPreRequisites(preRequ) {
+            this._preRequisites = preRequ;
+        }
+        public addPreRequisite(requ: Course) {
+            this._preRequisites[requ.getName()] = requ;
+        }
+        public removePreRequisite(requ: string) {
+            delete this._preRequisites[requ];
+        }
+    }
+
+    class CourseInstance extends Course {
+        private _professor: string;
+        private _semester: Semester;
+        private _year: number;
+        private _sectionNumber: string;
+        private _times;
+
+        //------------------
+
+        public getProfessor() {
+            return this._professor;
+        }
+        public setProfessor(professor: string) {
+            this._professor = professor;
+        }
+
+        public getSemester() {
+            return this._semester;
+        }
+        public setSemester(semester: Semester) {
+            this._semester = semester;
+        }
+
+        public getYear() {
+            return this._year;
+        }
+        public setYear(year: number) {
+            if (year > MINYEAR && year < MAXYEAR) {
+                this._year = year;
+            }
+        }
+
 
     }
 
