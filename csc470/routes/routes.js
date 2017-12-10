c95e04c62ccacbbcc9132f878e44b86f2fb8a385
CharlesSchimmel <charlesschimmel@gmail.com>
2017-12-10 15:17:12 -0600

    classes/current completely functional. I think.

--- csc470/routes/routes.js
+++ csc470/routes/routes.js
@@ -38,9 +38,8 @@ module.exports = function (app, passport) {
     app.get('/classes/current', isLoggedIn, function (req, res) {
         res.render('classes/current.ejs', { user_isloggedin: req.isAuthenticated(),
             user: req.user,
-            all_classes: req.user.classesInProgress,
-            classesWaitlisted: req.user.classesWaitlisted,
-            classesSignedUpfor: req.user.classesSignedUpfor,
+            all_classes: all_classes,
+            classes_needed: calcClassesNeeded(req.user)
         });
     });
     //classesFinished: [],
@@ -51,20 +50,20 @@ module.exports = function (app, passport) {
     app.get('/classes/taken', isLoggedIn, function (req, res) {
         res.render('classes/taken.ejs', { user_isloggedin: req.isAuthenticated(),
             user: req.user,
-            all_classes: req.user.classesFinished,
+            all_classes: req.user.classesFinished
         });
     });
     /////TRACK
     app.get('/classes/track', isLoggedIn, function (req, res) {
         res.render('classes/track.ejs', { user_isloggedin: req.isAuthenticated(),
             user: req.user,
-            credits_left: calcCreditsLeft(req.user),
+            credits_left: calcCreditsLeft(req.user)
         });
     });
     /////SCHEDULE
     app.get('/classes/schedule', isLoggedIn, function (req, res) {
         res.render('classes/schedule.ejs', { user_isloggedin: req.isAuthenticated(),
-            user: req.user,
+            user: req.user
         });
     });
     // ===================================================================================================================
@@ -124,31 +123,31 @@ module.exports = function (app, passport) {
     // same for taking, registered, waitlist, and untaken
     // "In Progress"
     app.post('/classes/take', function (req, res) {
-        req.user.classesInProgress.push(req.body.mark_taken);
+        req.user.classesInProgress.push(JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
     // "Still Needed"
     app.post('/classes/untake', function (req, res) {
-        untakeClass(req.user, req.body.mark_taken);
+        untakeClass(req.user, JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
     // "Completed"
     app.post('/classes/completed', function (req, res) {
-        req.user.classesFinished.push(req.body.mark_taken);
+        req.user.classesFinished.push(JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
     // "Waitlisted"
     app.post('/classes/waitlist', function (req, res) {
-        req.user.classesWaitlisted.push(req.body.mark_taken);
+        req.user.classesWaitlisted.push(JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
     // "Registered"
     app.post('/classes/register', function (req, res) {
-        req.user.classesSignedUpfor.push(req.body.mark_taken);
+        req.user.classesSignedUpfor.push(JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
@@ -178,6 +177,20 @@ function calcCreditsLeft(user) {
     }
     return count;
 }
+function calcClassesNeeded(user) {
+    var getName = function (c) { return c._name; };
+    var classesNeeded = [];
+    var notNeededClasses = user.classesFinished.concat(user.classesInProgress).concat(user.classesWaitlisted).concat(user.classesSignedUpfor);
+    var names = notNeededClasses.map(getName);
+    console.log(names);
+    for (var i = 0; i < all_classes.length; i++) {
+        if (names.indexOf(all_classes[i]._name) === -1) {
+            classesNeeded.push(all_classes[i]);
+        }
+    }
+    console.log(classesNeeded);
+    return classesNeeded;
+}
 function untakeClass(user, aClass) {
     var index = user.classesFinished.indexOf(aClass);
     if (index !== -1) {
@@ -195,5 +208,5 @@ function untakeClass(user, aClass) {
     if (index !== -1) {
         user.classesSignedUpfor.splice(index, 1);
     }
+    user.save();
 }
-//# sourceMappingURL=routes.js.map
--- csc470/routes/routes.ts
+++ csc470/routes/routes.ts
@@ -44,9 +44,8 @@ module.exports = function (app, passport) {
         res.render('classes/current.ejs', 
             { user_isloggedin: req.isAuthenticated(),
                 user: req.user,
-                all_classes: req.user.classesInProgress,
-                classesWaitlisted: req.user.classesWaitlisted,
-                classesSignedUpfor: req.user.classesSignedUpfor,
+                all_classes: all_classes,
+                classes_needed: calcClassesNeeded(req.user),
             }
         );
     });
@@ -150,35 +149,35 @@ module.exports = function (app, passport) {
 
     // "In Progress"
     app.post('/classes/take', function(req,res){
-        req.user.classesInProgress.push(req.body.mark_taken);
+        req.user.classesInProgress.push(JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
 
     // "Still Needed"
     app.post('/classes/untake', function (req,res){
-        untakeClass(req.user, req.body.mark_taken);
+        untakeClass(req.user, JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
 
     // "Completed"
     app.post('/classes/completed', function(req,res){
-        req.user.classesFinished.push(req.body.mark_taken);
+        req.user.classesFinished.push(JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
 
     // "Waitlisted"
     app.post('/classes/waitlist', function(req,res){
-        req.user.classesWaitlisted.push(req.body.mark_taken);
+        req.user.classesWaitlisted.push(JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
 
     // "Registered"
     app.post('/classes/register', function(req,res){
-        req.user.classesSignedUpfor.push(req.body.mark_taken);
+        req.user.classesSignedUpfor.push(JSON.parse(req.body.mark_taken));
         req.user.save();
         res.redirect('/classes/current');
     });
@@ -215,6 +214,22 @@ function calcCreditsLeft(user){
     }
     return count;
 }
+
+function calcClassesNeeded(user){
+    var getName = function(c){ return c._name; }
+    var classesNeeded=[];
+    var notNeededClasses = user.classesFinished.concat(user.classesInProgress).concat(user.classesWaitlisted).concat(user.classesSignedUpfor);
+    var names = notNeededClasses.map(getName);
+    console.log(names);
+    for (var i=0; i<all_classes.length; i++){
+        if (names.indexOf(all_classes[i]._name) === -1){
+            classesNeeded.push(all_classes[i]);
+        }
+    }
+    console.log(classesNeeded);
+    return classesNeeded;
+}
+
 function untakeClass(user,aClass){
     var index = user.classesFinished.indexOf(aClass);
     if (index !== -1){ user.classesFinished.splice(index,1) }
@@ -224,4 +239,5 @@ function untakeClass(user,aClass){
     if (index !== -1){ user.classesWaitlisted.splice(index,1) }
     index = user.classesSignedUpfor.indexOf(aClass);
     if (index !== -1){ user.classesSignedUpfor.splice(index,1) }
+    user.save();
 }
